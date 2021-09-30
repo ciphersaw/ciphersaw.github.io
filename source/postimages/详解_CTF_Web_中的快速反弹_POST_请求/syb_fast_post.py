840a1d4ba2d2ac74f71eeab8dc6817a5a9bcd4a4
ciphersaw <ciphersaw@gmail.com>
2021-09-30 16:41:55 +0800

    Refactor the directory of post images 1. Rename the directory from postimages to post-images. 2. Rename the subdirectories corresponding to the post names. 3. Rename the images by replacing underline with hyphen.

--- source/postimages/详解_CTF_Web_中的快速反弹_POST_请求/syb_fast_post.py
+++
@@ -1,9 +0,0 @@
-# Python 3.6
-import requests
-import base64 
-
-url = 'http://ctf5.shiyanbar.com/web/10/10.php'
-headers = requests.get(url).headers
-key = base64.b64decode(headers['FLAG']).decode().split(':')[1]
-post = {'key': key}
-print(requests.post(url, data = post).text)
